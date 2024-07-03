ce83e5a2b4c5101f82b51620ffc73c86159a4396
Magomeds05 <yusypovm@gmail.com>
2024-07-03 18:12:48 +0300

    Update control module 3.py

--- control module 3.py
+++ control module 3.py
@@ -8,34 +8,19 @@ data_structure = [
 
 def calculate_sum(data_structure):
     summa = 0
-    if isinstance(data_structure, (list, set, tuple)):
+    if isinstance(data_structure, str):
+        return len(data_structure)
+    elif isinstance(data_structure, (int, float)):
+        return data_structure
+    elif isinstance(data_structure, (list, set, tuple)):
         for i in data_structure:
             summa += calculate_sum(i)
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure. items():
+            summa += calculate_sum(key)
+            summa += calculate_sum(value)
+    return summa
 
 
-#def listsum(numList):
-   #if len(numList) == 1:
-        #return numList[0]
-   #else:
-        #return numList[0] + listsum(numList[1:])
-
-#print(listsum([1,2,3,4,5,6,7,8,2,35,26]))
-
-#def sum():
-    #theSum = 0
-    #for i in data_structure:
-        #theSum = theSum + i
-    #return theSum
-
-
-#sum = 0
-#for i in data_structure:
-    #sum += i
-    #print(f"Сумма цифр числа"[sum])
-
-
-
-#print(data_structure)
-#print(len(data_structure))
-#print(sum(data_structure[0]))
+print(calculate_sum(data_structure))
 
